95c8485ef35dfe64598166498f4962b5b9219a83
Lois <lois.regnier.93@gmail.com>
2017-04-20 18:09:26 +0200

    Fonctions update commentées

--- Modele/update.php
+++ Modele/update.php
@@ -1,32 +1,45 @@
 <?php
+    /**
+     * Génère une requête de mis à jour (UPDATE) avec les paramètres passé en paramètres AVEC une clause WHERE
+     *
+     * @param $table Nom de la table à mettre à jour
+     * @param $champsAMettreAJour Liste des champs à mettre à jour
+     * @param $nouvellesValeurs Nouvelles valeurs des champs. Respecter l'ordre des champs !
+     * @param $clauseWhere Clause WHERE mais sans le mot clé 'WHERE'
+     *
+     */
     function creerUpdate($table, $champsAMettreAJour, $nouvellesValeurs,$clauseWhere)
     {
-
-        echo "champsAMettreAJour : ".$champsAMettreAJour[0]."<br/>";
-        echo "nouvellesValeurs : ".$nouvellesValeurs[0]."<br/>";
-
+        //Récuèpere la taille des tableaux des champs et des nouvelles valeurs
         $sizeChamps = count($champsAMettreAJour);
         $sizeNVal = count($nouvellesValeurs);
 
+        //Vérifie que les tailles sont identiques et supérieurs à 0
         if($sizeChamps==$sizeNVal && $sizeNVal>0)
         {
             try {
                 //connexion
                 $bd = new PDO('mysql:host=172.31.21.41;dbname=lr206974;charset=utf8', 'lr206974', 'lr206974');
 
+                //Début de l'écriture de la requete
                 $requete = "UPDATE " . $table . " SET ";
 
+                //Pour tous les champs du tableau, on les ajoute à la requete avec leurs nouvelles valeurs
                 for($i=0;$i<$sizeChamps;$i++)
                 {
                     $requete.=$champsAMettreAJour[$i]."='".$nouvellesValeurs[$i]."'";
+
+                    //Ajoute une ',' si il reste des champs à mettre à jour ou un ' ' si c'est fini
                     if($i+1<$sizeChamps)
                         $requete.=',';
                     else
                         $requete.=' ';
                 }
 
+                //Rajout de la clause WHERE à la requete
                 $requete.=" WHERE ".$clauseWhere.";";
-                echo $requete;
+
+                //Execution de la requete
                 $bd->query($requete);
 
             } catch (Exception $e) {
@@ -39,33 +52,43 @@
         }
     }
 
+    /**
+     * Génère une requête de mis à jour (UPDATE) avec les paramètres passé en paramètres SANS la clause WHERE
+     *
+     * @param $table Nom de la table à mettre à jour
+     * @param $champsAMettreAJour Liste des champs à mettre à jour
+     * @param $nouvellesValeurs Nouvelles valeurs des champs. Respecter l'ordre des champs !
+     *
+     */
     function creerUpdateSansWhere($table, $champsAMettreAJour, $nouvellesValeurs)
     {
-
-        echo "champsAMettreAJour : ".$champsAMettreAJour[0]."<br/>";
-        echo "nouvellesValeurs : ".$nouvellesValeurs[0]."<br/>";
-
+        //Récuèpere la taille des tableaux des champs et des nouvelles valeurs
         $sizeChamps = count($champsAMettreAJour);
         $sizeNVal = count($nouvellesValeurs);
 
+        //Vérifie que les tailles sont identiques et supérieurs à 0
         if($sizeChamps==$sizeNVal && $sizeNVal>0)
         {
             try {
                 //connexion
                 $bd = new PDO('mysql:host=172.31.21.41;dbname=lr206974;charset=utf8', 'lr206974', 'lr206974');
 
+                //Début de l'écriture de la requete
                 $requete = "UPDATE " . $table . " SET ";
 
+                //Pour tous les champs du tableau, on les ajoute à la requete avec leurs nouvelles valeurs
                 for($i=0;$i<$sizeChamps;$i++)
                 {
                     $requete.=$champsAMettreAJour[$i]."='".$nouvellesValeurs[$i]."'";
+
+                    //Ajoute une ',' si il reste des champs à mettre à jour ou un ' ' si c'est fini
                     if($i+1<$sizeChamps)
                         $requete.=',';
                     else
                         $requete.=' ';
                 }
 
-                echo $requete;
+                //Execution de la requete
                 $bd->query($requete);
 
             } catch (Exception $e) {
@@ -77,9 +100,4 @@
             die('Erreur : taille des tableaux des champs et des nouvelles valeurs différentes');
         }
     }
-
-    $arrayupdate = array("nom","prenom");
-    $arrayNewVal = array("testNomSW","testPrenomSW");
-
-    creerUpdateSansWhere("COMPTE",$arrayupdate,$arrayNewVal);
 ?>
