d872bcf8ec2afc3d5093f4e3879089d9ddda376b
Reyxwe <paul.chapelon@hotmail.fr>
2017-04-24 19:00:07 +0200

    Update Carte.php

--- Modele/Carte.php
+++ Modele/Carte.php
@@ -9,35 +9,43 @@
 		private $xml_doc;
 		private $racine;
 		private $cmpE;
-		public function __construct()
+		private $admin;
+		private $lConsultant;
+        	private $lEditeur;
+
+
+		public function __construct($n,$dC,$dM,$p,$nbE,$xmlDoc,$r,$a,$lC,$lE)
 		{
-			$this->nom = 'Nouvelle carte';
-			$this->dateCreation = date("d-m-Y");
-			$this->dateModification = date("d-m-Y");
-			$this->publique = 'false';
-			$this->cmpE = 0;
-			$this->xml_doc = new DOMDocument('1.0', 'utf-8');
-			$this->racine = new Element('elt0','Racine',null,null);
-			//On ajoute la racine
-			$this->xml_doc->appendChild($this->xml_doc->createElement($this->racine->getId(),$this->racine->getValeur()));
-			
+			$this->nom = $n;
+			$this->dateCreation = $dC;
+			$this->dateModification = $dM;
+			$this->publique = $p;
+			$this->cmpE = $nbE;
+			$this->xml_doc = $xmlDoc;
+			$this->racine = $r;
+			$this->admin = $a;
+			$this->lConsultant = $lC;
+			$this->lEditeur = $lE;
+
 		}
-		
+
+
+		//Getter
 		public function getNom()
 		{
 			return $this->nom;
 		}
-		
+
 		public function getDateCreation()
 		{
 			return $this->dateCreation;
 		}
-		
+
 		public function getDateModification()
 		{
 			return $this->dateModification;
 		}
-		
+
 		public function getPublique()
 		{
 			return $this->publique;
@@ -46,7 +54,7 @@
 		{
 			return $this->cmpE;
 		}
-		
+
 		public function getXml_doc()
 		{
 			return $this->xml_doc;
@@ -55,25 +63,103 @@
 		{
 			return $this->racine;
 		}
-		
+
+		public function getAdmin()
+		{
+		    return $this->admin;
+		}
+
+			public function getListeConsultant()
+		{
+		    return $this->lConsultant;
+		}
+
+		public function getListeEditeur()
+		{
+		    return $this->lEditeur;
+		}
+
+
+        //Setter
 		public function setNom($n)
 		{
 			$this->nom = $n;
 		}
-		
+
 		public function setDateModification()
 		{
 			$this->dateModification = date("d-m-Y");
 		}
-		
+
 		public function setPublique($p)
 		{
 			$this->publique = $p;
 		}
-		
+
 		public function incCptE()
 		{
 			$this->cptE++;
 		}
+
+        public function remListeEditeur($editeur)
+        {
+            $indice =-1;
+            $indentation = 0;
+            foreach ($this->lEditeur as &$valeur)
+            {
+                if (strcmp($valeur,$editeur))
+                {
+                    $indice = $indentation;
+                }
+                $indentation ++;
+            }
+            if ($indice == -1)
+            {
+                return false;
+            }
+            else
+            {
+                unset($this->lEditeur[$indice]);
+                array_values($this->lEditeur); //réindexer le tableau
+                return true;
+            }
+        }
+
+        public function remListeConsultant($consultant)
+        {
+            $indice =-1;
+            $indentation = 0;
+            foreach ($this->lConsultant as &$valeur)
+            {
+                if (strcmp($valeur,$consultant))
+                {
+                    $indice = $indentation;
+                }
+                $indentation ++;
+            }
+            if ($indice == -1)
+            {
+                return false;
+            }
+            else
+            {
+                unset($this->lConsultant[$indice]);
+                array_values($this->lConsultant); //réindexer le tableau
+                return true;
+            }
+        }
+
+
+
+        public function addListeEditeur($editeur)
+        {
+            array_push($this->lEditeur, $editeur);
+        }
+
+        public function addListeConsultant($consultant)
+        {
+            array_push($this->lConsultant,$consultant);
+        }
+
 	}
 ?>
